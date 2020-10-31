f84362bdd76bbc8a617b105a690ef9b0583671e7
stanislav <staskorol21@gmail.com>
2020-10-31 20:03:27 +0300

    bug fixes viewModelProviders

--- app/src/main/java/ru/skillbranch/skillarticles/viewmodels/base/ViewModelDelegate.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/base/ViewModelDelegate.kt
@@ -12,8 +12,8 @@ class ViewModelDelegate<T : ViewModel>(private val clazz: Class<T>, private val 
     private lateinit var value: T
     override fun getValue(thisRef: FragmentActivity, property: KProperty<*>): T {
         if (!::value.isInitialized) value = when (arg) {
-            null -> ViewModelProvider(thisRef).get(clazz)
-            else -> ViewModelProvider(thisRef,
+            null -> ViewModelProviders.of(thisRef).get(clazz)
+            else -> ViewModelProviders.of(thisRef,
                 ViewModelFactory(arg)
             ).get(clazz)
         }
